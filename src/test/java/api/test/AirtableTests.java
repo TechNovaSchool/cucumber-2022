db27ec105bfa9cb62a3fbceb6bf66a92e58ef451
andrewnova <tech.easy1@outlook.com>
2022-07-16 14:19:52 -0500

    adding delete

--- src/test/java/api/test/AirtableTests.java
+++ src/test/java/api/test/AirtableTests.java
@@ -8,7 +8,8 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
-import org.junit.Test;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 import utilities.Config;
 
 import java.util.ArrayList;
@@ -16,7 +17,7 @@ import java.util.List;
 
 public class AirtableTests {
 
-    @Test
+    @Test (priority = 1)
     public void getRecord() throws Exception {
         Response response = RestAssured.given()
                 .header("Authorization", "Bearer keyUciDKN0atCXT7w")
@@ -31,7 +32,8 @@ public class AirtableTests {
 
         String len = rb.getRecords().toString();
         int size = rb.getRecords().size();
-        System.out.println(size);
+        Assert.assertEquals(response.statusCode(), 200 );
+//        System.out.println(size);
 
         for (Record elements : rb.getRecords()) {
             if (elements.getFields().getName().startsWith("A")) {
@@ -59,8 +61,9 @@ public class AirtableTests {
 //            }
 //        }
     }
+    String myRecordId;
 
-    @Test
+    @Test (priority = 2)
     public void postRecord() throws JsonProcessingException {
 //
         MyFields fields = new MyFields();
@@ -81,7 +84,7 @@ public class AirtableTests {
 
         ObjectMapper objectMapper = new ObjectMapper();
         String jsonValue = objectMapper.writeValueAsString(requestBody);
-        System.out.println(jsonValue);
+//        System.out.println(jsonValue);
 
 
         Response response = RestAssured.given()
@@ -90,18 +93,23 @@ public class AirtableTests {
                 .contentType("application/json")
                 .body(jsonValue)
                 .post(Config.getProperty("host"));
-        System.out.println(response.statusCode());
+//        System.out.println(response.statusCode());
+
+        ResponseBody rb = objectMapper.readValue(response.asString(), ResponseBody.class);
+        myRecordId = rb.getRecords().get(0).getId();
+        Assert.assertEquals(response.statusCode(), 200 );
+
 
     }
 
-    @Test
+    @Test (priority = 3)
     public void updateRecord() throws JsonProcessingException {
         MyFields fields = new MyFields();
         fields.setName("JamesBond");
 
         Record record = new Record();
         record.setFields(fields);
-        record.setId("recbxWmbMxmAxKWS1");
+        record.setId(myRecordId);
 
         List<Record> records = new ArrayList<>();
         records.add(record);
@@ -111,15 +119,36 @@ public class AirtableTests {
 
         ObjectMapper objectMapper = new ObjectMapper();
         String jsonValue = objectMapper.writeValueAsString(requestBody);
-        System.out.println(jsonValue);
+//        System.out.println(jsonValue);
 
         Response response = RestAssured.given()
                 .header("Authorization", "Bearer keyUciDKN0atCXT7w")
                 .urlEncodingEnabled(false)
-                .contentType("application/json")
                 .body(jsonValue)
+                .contentType("application/json")
                 .patch(Config.getProperty("host"));
-        System.out.println(response.statusCode());
+
+//        System.out.println(response.statusCode());
+        Assert.assertEquals(response.statusCode(), 200 );
+
+    }
+
+    @Test (priority = 4)
+    public void deleteRecord() {
+
+        String queryPram = "records[]";
+        String recordID = myRecordId;
+
+        Response response = RestAssured.given()
+                .header("Authorization", "Bearer keyUciDKN0atCXT7w")
+                .urlEncodingEnabled(false)
+                .queryParam(queryPram, recordID)
+                .delete(Config.getProperty("host"));
+//        System.out.println(response.statusCode());
+//        System.out.println(response.asString());
+        Assert.assertEquals(response.statusCode(), 200 );
+
+
 
     }
 
