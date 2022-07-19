6b3651c9e281adc9ef620b491fd6e36b4cee43fb
andrewnova <tech.easy1@outlook.com>
2022-07-18 20:36:27 -0500

    utilities

--- src/test/java/api/test/AirtableUtilTest.java
+++ src/test/java/api/test/AirtableUtilTest.java
@@ -1,7 +1,13 @@
 package api.test;
 
+import api.models.MyFields;
+import api.models.Record;
+import api.models.RequestBody;
 import org.testng.annotations.Test;
 import utilities.APIUtil;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class AirtableUtilTest {
 
@@ -9,5 +15,27 @@ public class AirtableUtilTest {
     public void testGetMethod() {
         String resource = "/Table%201";
         APIUtil.hitGET(resource);
+    }
+
+    @Test
+    public void testPostMethod() {
+        String resource = "/Table%201";
+
+        MyFields fields = new MyFields();
+        fields.setName("MAX");
+        fields.setEmail("test@gmail.com");
+        fields.setAddress("985 road");
+        fields.setNotes("these are my notes");
+        fields.setPhone("123-123-4569");
+        fields.setCheckBox(false);
+
+        Record record = new Record();
+        record.setFields(fields);
+        List<Record> records = new ArrayList<>();
+        records.add(record);
+        RequestBody requestBody = new RequestBody();
+        requestBody.setRecords(records);
+
+        APIUtil.hitPOST(resource,requestBody);
     }
 }
--- src/test/java/utilities/APIUtil.java
+++ src/test/java/utilities/APIUtil.java
@@ -1,19 +1,24 @@
 package utilities;
 
+import api.models.RequestBody;
 import api.models.ResponseBody;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 
 public class APIUtil {
     //CRUD operations
     //GET, POST, PATCH, DELETE
 
+    private static Response response;
+    private static ResponseBody responseBody;
+
 
     public static void hitGET(String resource) {
         String uri = Config.getProperty("host") + resource;
-        Response response = RestAssured.given()
+        response = RestAssured.given()
                 .header("Authorization", "Bearer " + Config.getProperty("token"))
                 .urlEncodingEnabled(false)
                 .get(uri);
@@ -22,12 +27,80 @@ public class APIUtil {
         ObjectMapper objectMapper = new ObjectMapper();
 
         try {
-            ResponseBody responseBody = objectMapper.readValue(response.asString(), ResponseBody.class);
+            responseBody = objectMapper.readValue(response.asString(), ResponseBody.class);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void hitPOST(String resource, RequestBody body) {
+        String uri = Config.getProperty("host") + resource;
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        String bodyJSON = "";
+        try {
+            bodyJSON = objectMapper.writeValueAsString(body);
         } catch (JsonProcessingException e) {
             e.printStackTrace();
         }
 
+        response = RestAssured.given()
+                .header("Authorization", "Bearer " + Config.getProperty("token"))
+                .urlEncodingEnabled(false)
+                .contentType(ContentType.JSON)
+                .body(bodyJSON)
+                .post(uri);
+        System.out.println(response.statusCode());
 
+        try {
+            responseBody = objectMapper.readValue(response.asString(), ResponseBody.class);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void hitPATCH(String resource, RequestBody body) {
+        String uri = Config.getProperty("host") + resource;
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        String bodyJSON = "";
+        try {
+            bodyJSON = objectMapper.writeValueAsString(body);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+
+        response = RestAssured.given()
+                .header("Authorization", "Bearer " + Config.getProperty("token"))
+                .urlEncodingEnabled(false)
+                .contentType(ContentType.JSON)
+                .body(bodyJSON)
+                .patch(uri);
+        System.out.println(response.statusCode());
+
+        try {
+            responseBody = objectMapper.readValue(response.asString(), ResponseBody.class);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void hitDELETE(String resource,String recordId) {
+        String uri = Config.getProperty("host") + resource;
+
+        response = RestAssured.given()
+                .header("Authorization", "Bearer " + Config.getProperty("token"))
+                .urlEncodingEnabled(false)
+                .queryParam("records[]", recordId)
+                .delete(uri);
+        System.out.println(response.statusCode());
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        try {
+            responseBody = objectMapper.readValue(response.asString(), ResponseBody.class);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
     }
 
 
