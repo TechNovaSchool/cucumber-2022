5a281099877c79a52d674532af0654ed6bffb757
andrewnova <tech.easy1@outlook.com>
2022-07-20 20:15:40 -0500

    hybrid

--- src/test/java/steps/Airtable_Hybrid_steps.java
+++ src/test/java/steps/Airtable_Hybrid_steps.java
@@ -3,17 +3,22 @@ package steps;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.Assert;
 import pages.AirtableLoginPage;
+import pages.AirtableOnePage;
+import utilities.APIUtil;
 import utilities.Config;
 import utilities.Driver;
 
 public class Airtable_Hybrid_steps {
     AirtableLoginPage airtableLoginPage = new AirtableLoginPage();
+    AirtableOnePage tablePage = new AirtableOnePage();
+    String name = "Andrew";
 
     @Given("Navigate to login page")
     public void navigate_to_login_page() {
         Driver.getDriver().get(Config.getProperty("loginAirTableURL"));
-        airtableLoginPage.signInReference.click();
+//        airtableLoginPage.signInReference.click();
     }
     @When("Enter the email address")
     public void enter_the_email_address() {
@@ -29,18 +34,25 @@ public class Airtable_Hybrid_steps {
     }
     @Then("Home base page should appear")
     public void home_base_page_should_appear() {
-
+    String expectedValue = "My First Workspace";
+    String actualValue = tablePage.workSpaceName.getText();
+        Assert.assertEquals(expectedValue, actualValue);
     }
     @Then("Click on Transformers button")
     public void click_on_transformers_button() {
-
+        airtableLoginPage.getTransformersTable.click();
     }
     @When("I add a new record")
     public void i_add_a_new_record() {
+        tablePage.plusSign.click();
+        tablePage.plusSign.sendKeys(name);
 
     }
     @Then("I will check with API that record was saved")
     public void i_will_check_with_api_that_record_was_saved() {
-
+        String resource = "/table%201";
+        APIUtil.hitGET(resource);
+        String actualName =  APIUtil.getResponseBody().getRecords().get(0).getFields().getName();
+        Assert.assertEquals(actualName, name);
     }
 }
